487e18c47f15a4535bbb77066689c5616fceb781
Drew Foxall <64457387+drewfoxall@users.noreply.github.com>
2024-05-26 22:39:53 -0400

    feat: add range feature

---
+++ Examples/range/range.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	nums := []int{2,3,4}
+	sum := 0
+
+	for _, num := range nums{
+		sum += num
+	}
+	fmt.Println(sum)
+
+	kvs := map[string]string{"a": "apple", "b": "banana"}
+	for k,v := range kvs {
+		fmt.Printf(" %s -> %s ", k, v)
+	}
+}
